d134c88b54ea3044f10fc0b677a89130e606a320
Samuel Adu <samuel.adu@gmail.com>
2019-07-14 15:07:20 +0100

    Create greeter.py

---
+++ src/greeter.py
@@ -0,0 +1,21 @@
+
+'''
+greeter.py
+
+Sends greetings to one and all.
+'''
+import sys
+
+
+def greet(name):
+    print('Hello %s!' % name)
+
+def complain():
+    print('Hey - you have to tell me your name!')
+    
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        complain()
+    else:
+        greet(sys.argv[1])
